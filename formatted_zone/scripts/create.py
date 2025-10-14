4baad98c6dc0df753a09a68ae3ef5c6fd1f67255
Whitebear <pellehartvigandersen@gmail.com>
2025-10-14 16:01:22 +0200

    create formatted zone buckets

---
+++ formatted_zone/scripts/create.py
@@ -0,0 +1,32 @@
+import boto3
+import logging
+from utils import *
+from consts import * 
+
+setup_logging("create.log")
+
+def main():
+
+    # MinIO client connection, using Amazon S3 API and boto3 Python library
+    try:
+        s3_client = boto3.client(
+            "s3",
+            endpoint_url=ENDPOINT_URL,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        )
+        logging.info("Connected to MinIO.")
+
+        # Create the bucket and the sub-buckets
+        create_bucket(s3_client, FORMATTED_ZONE_BUCKET)
+        create_sub_bucket(s3_client, FORMATTED_ZONE_BUCKET, TEMPORAL_SUB_BUCKET)
+        create_sub_bucket(s3_client, FORMATTED_ZONE_BUCKET, PERSISTENT_SUB_BUCKET)
+
+    except Exception:
+        logging.exception("Error connecting to MinIO.")
+        return
+    
+
+
+if __name__ == "__main__":
+    main()
