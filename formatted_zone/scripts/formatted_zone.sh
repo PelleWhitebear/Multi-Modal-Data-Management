dc76be0d1df99c3fc112c84ffda818599fb4c517
diego.velilla <diegovelillarecio@gmail.com>
2025-10-18 19:15:35 +0200

    formated -> formatted ☝ 🤓️

--- formatted_zone/scripts/formatted_zone.sh
+++ formatted_zone/scripts/formatted_zone.sh
@@ -3,25 +3,25 @@ set -e
 
 echo "Starting Formatted Zone processing..."
 create_t0=$(date +%s)
-python3 create.py
+python3 formatted_zone/scripts/create.py
 create_t1=$(date +%s)
 echo "Formatted Zone buckets created."
 
 echo "Formatting JSON files..."
 format_json_t0=$(date +%s)
-python3 format_json.py
+python3 formatted_zone/scripts/format_json.py
 format_json_t1=$(date +%s)
 echo "JSON formatting completed."
 
 echo "Formatting image files..."
 format_images_t0=$(date +%s)
-python3 format_images.py
+python3 formatted_zone/scripts/format_images.py
 format_images_t1=$(date +%s)
 echo "Image formatting completed."
 
 echo "Formatting video files..."
 format_videos_t0=$(date +%s)
-python3 format_videos.py
+python3 formatted_zone/scripts/format_videos.py
 format_videos_t1=$(date +%s)
 echo "Video formatting completed."
 
