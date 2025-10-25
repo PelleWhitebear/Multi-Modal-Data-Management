2c54012c06dbff6478aa23e579d3e31f6e09f47b
AlexBuenoL <abuenoleon@gmail.com>
2025-10-25 15:53:20 +0200

    image quality

--- trusted_zone/process_images.py
+++ trusted_zone/process_images.py
@@ -2,7 +2,7 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 from io import BytesIO
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageOps
 import dotenv
 import os
 from global_scripts.utils import create_bucket, create_sub_bucket
@@ -32,18 +32,28 @@ def process_images(s3_client, formatted_zone_prefix, trusted_zone_prefix):
                 # Get the image from MinIO
                 response = s3_client.get_object(Bucket=os.getenv("FORMATTED_ZONE_BUCKET"), Key=key)
                 file_content = response['Body'].read()
+
+                # Open image and ensure is not corrupted
                 img = Image.open(BytesIO(file_content))
+
+                # All images have the same channels (without A)
                 img = img.convert('RGB')
-                img = img.resize((256, 256))
-                enhancer = ImageEnhance.Brightness(img)
-                img = enhancer.enhance(1.0)
+
+                # Standardize brightness with Histogram Equalization
+                img = ImageOps.equalize(img)
+
+                # Standardize image resolution
+                img = ImageOps.pad(img, (256, 256))
+
                 # Save to buffer (removes metadata)
                 buffer = BytesIO()
                 img.save(buffer, format='JPEG')
                 buffer.seek(0)
+
                 # Define new key for trusted zone
                 base_name = key.split('/')[-1]
                 new_key = f"{trusted_zone_prefix}/{base_name}"
+
                 # Upload to trusted zone
                 s3_client.put_object(
                     Bucket=os.getenv("TRUSTED_ZONE_BUCKET"),
@@ -72,8 +82,8 @@ def main():
         logging.info("Connected to MinIO.")
         process_images(
             s3_client,
-            formatted_zone_prefix="media/image",
-            trusted_zone_prefix="media/image"
+            formatted_zone_prefix="media/image/",
+            trusted_zone_prefix="media/image/"
         )
         logging.info("Image processing completed.")
     except ClientError as e:
